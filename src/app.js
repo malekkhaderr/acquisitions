b59d81866b2759b3aeccf797a37bf5e724b81810
malekkhaderr <malekkhader@outlook.com>
2026-08-20 03:30:08 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+//setting up the express application with the right middleware
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from acquisitions!");
+});
+
+export default app;
